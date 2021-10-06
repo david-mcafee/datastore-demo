d56933fbf2a1267fb777be0ec0584735297b4822
David McAfee <mcafd@amazon.com>
2021-10-06 13:41:07 -0700

    update

--- src/Components/Nav/index.tsx
+++ src/Components/Nav/index.tsx
@@ -1,7 +1,5 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Menu } from "semantic-ui-react";
-import { Analytics } from "aws-amplify";
+import { Menu } from "semantic-ui-react";
 import { AmplifySignOut } from "@aws-amplify/ui-react";
 import { useStyles } from "./styles";
 
@@ -12,33 +10,11 @@ type NavProps = {
 const Nav = ({ username }: NavProps) => {
   const { navContainer } = useStyles();
 
-  function testAnalyticsButton() {
-    Analytics.record({
-      name: "test button click",
-      metrics: { clickNumber: 1 },
-    });
-  }
-
   return (
     <Menu className={navContainer}>
       <Menu.Item header>{`Welcome ${username}!`}</Menu.Item>
       <Menu.Item>
         <Link to="/">DataStore</Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link to="/chatbot">Chatbot</Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link to="/lambda">Lambda</Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link to="/pubsub">PubSub</Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link to="/storage">Storage</Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Button onClick={testAnalyticsButton}>Test Analytics</Button>
       </Menu.Item>
       <Menu.Item>
         <AmplifySignOut />
